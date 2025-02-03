e7a67086699c34657979bae8a2b7eb733944630b
Nevena <nevena.rogova@gmx.at>
2025-02-03 22:46:55 +0200

    Add files via upload

---
+++ foodDelivery.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+double chickenMenue = 10.35;
+double fishMenue = 12.40;
+double vegiMenue = 8.15;
+double priceDelivery = 2.50;
+
+int numberChickenMenues = int.Parse(Console.ReadLine());
+int numberFishMenues = int.Parse(Console.ReadLine());
+int numberVegiMenues = int.Parse(Console.ReadLine());
+
+double priceChichenMenues = chickenMenue * numberChickenMenues;
+double priceFishMenue = fishMenue * numberFishMenues;
+double priceVegiMenue = vegiMenue * numberVegiMenues;
+double priceMenues = priceChichenMenues + priceFishMenue + priceVegiMenue;
+double priceDesert = priceMenues * 0.20;
+double totalSum = priceMenues + priceDesert + priceDelivery;
+Console.WriteLine(totalSum);
---
+++ radiansToDegrees.cs
@@ -0,0 +1,3 @@
+﻿double radians = double.Parse(Console.ReadLine());
+double degreese = radians * 180 / Math.PI;
+Console.WriteLine(degreese);
