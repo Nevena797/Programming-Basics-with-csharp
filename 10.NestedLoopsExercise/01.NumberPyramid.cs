cb115f24682fdc12f9f23a66807a31c73b2a8dc5
Nevena <nevena.rogova@gmx.at>
2025-03-27 08:54:15 +0200

    Add files via upload

---
+++ 10.NestedLoopsExercise/01.NumberPyramid.cs
@@ -0,0 +1,21 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+
+int row = 1;
+int col = 1;
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i} ");
+
+    if (row == col)
+    {
+        row++;
+        col = 1;
+        Console.WriteLine();
+    }
+    else
+    {
+        col++;
+    }
+}
---
+++ 10.NestedLoopsExercise/01.NumberPyramid1/01NumberPyramid1.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int number = 1;
+for (int row = 1; row <= n; row++)
+{
+    for (int col = 1; col <= row; col++)
+    {
+        Console.Write($"{number++} ");
+
+        if (number > n)
+        {
+            return;
+        }
+    }
+    Console.WriteLine();
+}
