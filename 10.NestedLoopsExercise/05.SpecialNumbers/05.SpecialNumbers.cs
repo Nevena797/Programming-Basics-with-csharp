824546adfc3d7f39ac7842c81913f5669aa90e44
Nevena <nevena.rogova@gmx.at>
2025-04-08 15:43:45 +0300

    Add files via upload

---
+++ 10.NestedLoopsExercise/05.SpecialNumbers/05.SpecialNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int number = 1111; number <= 9999; number++)
+            {
+                string numStr = number.ToString();
+                bool isSpecial = true;
+
+                foreach (char digitChar in numStr)
+                {
+                    int digit = digitChar - '0';
+
+                    if (digit == 0 || n % digit != 0)
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+
+                if (isSpecial)
+                {
+                    Console.Write(number + " ");
+                }
+            }
+        }
+    }
+}
